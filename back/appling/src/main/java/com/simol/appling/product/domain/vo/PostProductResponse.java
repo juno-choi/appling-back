498d4cc4828419944c9841d7145b6bf3f3620fc4
juno-choi <ililil9482@naver.com>
2024-09-09 09:35:13 +0900

    feat: 반환 데이터 추가

--- back/appling/src/main/java/com/simol/appling/product/domain/vo/PostProductResponse.java
+++ back/appling/src/main/java/com/simol/appling/product/domain/vo/PostProductResponse.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.simol.appling.product.domain.entity.ProductEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder
+@Schema(description = "상품 등록 반환 데이터")
 public record PostProductResponse(
-    Long productId
+    @Schema(description = "상품번호", example = "1") Long productId
 ) {
     public static PostProductResponse createFrom(ProductEntity saveProduct) {
         return PostProductResponse.builder()
--- back/appling/src/main/java/com/simol/appling/product/domain/vo/ProductListResponse.java
+++ back/appling/src/main/java/com/simol/appling/product/domain/vo/ProductListResponse.java
@@ -3,6 +3,7 @@ package com.simol.appling.product.domain.vo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
@@ -10,11 +11,18 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Builder
+@Schema(description = "상품리스트")
 public record ProductListResponse(
+        @Schema(description = "페이지 마지막 여부", example = "true")
         Boolean last,
+        @Schema(description = "페이지 비어있음 여부", example = "false")
         Boolean empty,
+        @Schema(description = "총 페이지", example = "1")
         int totalPage,
+        @Schema(description = "총 데이터 수", example = "1")
         Long totalElements,
+        @Schema(description = "페이지에 존재하는 데이터 수", example = "1")
         int numberOfElement,
+        @Schema(description = "리스트")
         List<ProductVo> productList
 ){}
