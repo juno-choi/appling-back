b9d870eb6c08c69f21b7731d8ee45b2ea955ba86
juno-choi <ililil9482@naver.com>
2024-09-24 07:41:22 +0900

    feat: cors 처리

---
+++ src/main/java/com/simol/appling/global/web/WebConfig.java
@@ -0,0 +1,17 @@
+package com.simol.appling.global.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                ;
+    }
+}
